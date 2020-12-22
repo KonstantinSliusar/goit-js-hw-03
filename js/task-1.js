25b053e8dadaf2f9dd0aafad94a80c44e8e6183d
KonstantinSliusar <61802266+KonstantinSliusar@users.noreply.github.com>
2020-12-22 17:44:08 +0200

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -3,22 +3,19 @@ const user = {
     age: 20,
     hobby: 'html',
     premium: true,
-    mood: 'happy',
-    newUser(){
-        console.log(this)
-        this.hobby = 'skydiving';
-        this.premium = false;
-        const keys = Object.keys(user);
-        // console.log(keys)
-        for (const key of keys){
-            console.log(key);
-            // console.log(this);  
-        }
-    }
 };
 
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+for(const key in user){
+    // console.log(`${key}: ${user[key]}`)
+}
+const userKeys = Object.keys(user);
 
-user.newUser();
+for(const key of userKeys){
+    console.log(`${key}: ${user[key]}`)
+}
 
 
 
